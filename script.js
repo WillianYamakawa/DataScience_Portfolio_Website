42ce63e22af439a6b29bb1b86de2adc1e977d743
Willian Yamakawa <willianysouza2003@gmail.com>
2022-06-27 19:24:55 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-let fileurl = window.location.hash.replace("#/", "")
-fileurl = `./content/${fileurl}/index.html`
-fetch(fileurl).then(value => value.text()).then(html => document.getElementById("content").innerHTML = html)
+let fileurl = window.location.hash.replace("#/", "");
+
+if(fileurl === ""){
+	document.getElementById("content").innerHTML = '<h1 class="title loading">Site inexistente</h1>'
+}else{
+	getHtml(fileurl)
+}
+
+
+function getHtml(file){
+	file = `./content/${file}/index.html`;
+
+fetch(file)
+	.then((value) => {
+		if (value.ok) {
+			return value.text();
+		} else {
+			throw "err";
+		}
+	})
+	.then((html) => (document.getElementById("content").innerHTML = html))
+	.catch((err) => {
+        
+    });
+}
